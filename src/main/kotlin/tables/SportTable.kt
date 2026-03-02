3a86017891f551090a53ad524cf39589ab4bc836
Daniel L. Tobing <ninacinta206@gmail.com>
2026-03-02 07:50:11 +0700

    push

--- src/main/kotlin/tables/SportTable.kt
+++ src/main/kotlin/tables/SportTable.kt
@@ -3,7 +3,7 @@ package org.delcom.tables
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
-object SportTable : UUIDTable("plants") {
+object SportTable : UUIDTable("sports") {
     val nama = varchar("nama", 100)
     val pathGambar = varchar("path_gambar", 255)
     val deskripsi = text("deskripsi")
