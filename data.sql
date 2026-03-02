3494433c0522ffcae6102d79ac199685d0210d52
Daniel L. Tobing <ninacinta206@gmail.com>
2026-03-02 10:43:26 +0700

    push

--- data.sql
+++ data.sql
@@ -9,3 +9,16 @@ CREATE TABLE IF NOT EXISTS plants (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS sports (
+                                      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(255) NOT NULL,
+    path_gambar VARCHAR(255) NOT NULL,
+    deskripsi TEXT NOT NULL,
+    kelebihan TEXT NOT NULL,
+    teknologi TEXT NOT NULL,
+    tenaga TEXT  NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
--- src/main/kotlin/tables/SportTable.kt
+++ src/main/kotlin/tables/SportTable.kt
@@ -7,7 +7,7 @@ object SportTable : UUIDTable("sports") {
     val nama = varchar("nama", 100)
     val pathGambar = varchar("path_gambar", 255)
     val deskripsi = text("deskripsi")
-    val kelebihan = text("kelebihan"
+    val kelebihan = text("kelebihan")
     val teknologi = text("teknologi")
     val tenaga = text("tenaga")
     val createdAt = timestamp("created_at")
