48f682b8bf78a3b6610827a4af7f9daf9ed9ac25
Daniel L. Tobing <ninacinta206@gmail.com>
2026-03-02 09:51:38 +0700

    push

--- src/main/kotlin/tables/SportTable.kt
+++ src/main/kotlin/tables/SportTable.kt
@@ -7,8 +7,8 @@ object SportTable : UUIDTable("sports") {
     val nama = varchar("nama", 100)
     val pathGambar = varchar("path_gambar", 255)
     val deskripsi = text("deskripsi")
-    val kelebihan = text("kelebihan")
-    val teknologi = text("teknologi_yang_digunkan")
+    val kelebihan = text("kelebihan"
+    val teknologi = text("teknologi")
     val tenaga = text("tenaga")
     val createdAt = timestamp("created_at")
     val updatedAt = timestamp("updated_at")
